1d53efb119f66a000275e5ac3d26874a2f000ac3
dlentz92 <dlentz92@gmail.com>
2020-02-03 15:41:25 -0500

    html set up. variables set up

---
+++ app.js
@@ -0,0 +1,18 @@
+var amount = document.querySelector("#amount");
+var interest = document.querySelector("#interest");
+var years = document.querySelector("#years");
+var monthlyPayment = document.querySelector("#monthly-payment");
+var totalPayment = document.querySelector("#total-payment");
+var totalInterest = document.querySelector("#total-interest")
+
+// TOTAL INTEREST CALCULATION
+function interestCalc(interest){
+    let newInterest = interest * .01
+// I = PRT
+
+}
+
+// TOTAL PAYMENT CALCULATION
+
+
+// MONTHLY PAYMENT
