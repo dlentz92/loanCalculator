d031e7d93b90b69fed5050d4681ba0c0a0cb986e
dlentz92 <dlentz92@gmail.com>
2020-02-03 16:34:26 -0500

    basic outline complete. JS needs to begin

--- app.js
+++ app.js
@@ -1,18 +1,55 @@
-var amount = document.querySelector("#amount");
-var interest = document.querySelector("#interest");
-var years = document.querySelector("#years");
-var monthlyPayment = document.querySelector("#monthly-payment");
-var totalPayment = document.querySelector("#total-payment");
-var totalInterest = document.querySelector("#total-interest")
+var amount = document.getElementById("#amount");
+var interest = document.getElementById("#interest");
+var years = document.getElementById("#years");
+var submit = document.getElementById("#submit")
+var monthlyPayment = document.getElementById("#monthly-payment");
+var totalPayment = document.getElementById("#total-payment");
+var totalInterest = document.getElementById("#total-interest")
+
+
+
 
 // TOTAL INTEREST CALCULATION
 function interestCalc(interest){
     let newInterest = interest * .01
 // I = PRT
 
+totalInterest = amount*newInterest*years
+totalInterest.innerHTML = totalInterest
+
+
 }
+console.log(interestCalc(10))
+
 
 // TOTAL PAYMENT CALCULATION
 
+function totalCalc(amount){
+    let newPayment;
+    newPayment = amount + interestCalc()
+
+}
 
 // MONTHLY PAYMENT
+
+function monthlyCalc(years){
+    let monthlyPay;
+    monthlyPay = Math.floor(totalCalc()/12)
+    monthlyPay.innerHTML = monthlyPay
+}
+
+
+// ROLL UP
+submit.addEventListener("click", function rollUp(){
+interestCalc();
+paymentCalc();
+monthlyCalc();
+
+submit.addEventListener("click", function(){
+    event.preventDefault()
+})
+
+})
+
+
+
